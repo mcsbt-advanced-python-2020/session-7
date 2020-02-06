2c2076a875d50341f8f7d2edaba9604449115c7c
Pepe García <pepe@pepegar.com>
2020-02-06 09:34:33 +0100

    changes in class

--- example-2-callbacks/simple_callback.py
+++ example-2-callbacks/simple_callback.py
@@ -8,15 +8,20 @@ app = dash.Dash(__name__)
 
 app.layout = html.Div(children = [
   html.H1("Using callbacks"),
-  dcc.Input(id="textbox-changed", type="text"),
+  dcc.Dropdown(
+      id="dropdown",
+      options= [
+          {"value": "whatever2", "label": "one"},
+          {"value": "whatever", "label": "two"}
+      ]),
   html.H2(id="id-changes")
 ])
 
 @app.callback(
   Output(component_id="id-changes",component_property="children"),
-  [Input(component_id="textbox-changed", component_property="value")]
+  [Input(component_id="dropdown", component_property="value")]
 )
-def change_h2_on_input_change(value):
-    return value
+def change_h2_on_input_change(potato):
+    return "the value of the input is: " + potato
 
 app.run_server(debug=True)
